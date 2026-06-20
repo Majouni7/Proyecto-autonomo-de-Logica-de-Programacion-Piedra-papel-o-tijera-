d21fb5adcddd97345a4cebf79b669ac5dc90a76b
Majouni7 <majo7.uni@hotmail.com>
2026-06-20 16:36:24 -0500

    cambios

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 ##Juego de piedra, papel o tijera##
 
-
 ##Variable que hace posible el bucle##
 continuar = "si"
 
 
 ##El bucle permite repetir el juego si el usuario lo desea##
 while continuar == "si":
-
-
+    
     ##El jugador y el sistema eligen una opción##
     jugador = input("Elige piedra, papel o tijera: ")
     sistema = input("Ingrese la opción del sistema: ")
@@ -31,14 +29,9 @@ while continuar == "si":
         print("Perdiste")
 
 
-##Se pregunta al jugador si desea volver a jugar##
-    volver_a_la_partida = input("¿Desea jugar de nuevo? ")
+##Se pregunta al jugador si desea volver a jugar para aplicar el bucle##
+    continuar = input("¿Desea jugar de nuevo? (si/no): ")
     
-##En esta parte se aplica el bucle para volver a jugar
-    if volver_a_la_partida == "si":
-        continuar = "si"
-
-##En este caso no se aplicaria el bucle y se terminaria el juego##
-    elif volver_a_la_partida =="no":
-        continuar = "no"
-        print ("Salir del juego")
+##Si el usuario deside no seguir jugando, finaliza el juego ##
+    if continuar == "no":
+        print("Salir del juego")
